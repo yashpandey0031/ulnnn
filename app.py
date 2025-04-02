a45b04f70807ab7b5f61ea4b60d87b74d2efb87e
Tanazza <140501011+tanazzamuskan@users.noreply.github.com>
2025-04-03 00:37:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ from pages import home, about, assessment, resources, profile
 
 st.set_page_config(page_title="Mental Health App", layout="wide")
 
+# If sidebar isn't needed, remove its content
+for _ in range(10):  
+    st.sidebar.empty()
+
 # Sidebar Navigation
 st.sidebar.title("Navigation")
-selected_page = st.sidebar.radio("Go to", ["Home", "About", "Assessment", "Resources", "Profile"])
+selected_page = st.sidebar.radio("Go to", ["Home", "Assessment", "Resources","About Us"])
 
 # Load the selected page
 if selected_page == "Home":
     home.show()
-elif selected_page == "About":
+elif selected_page == "About Us":
     about.show()
 elif selected_page == "Assessment":
     assessment.show()
 elif selected_page == "Resources":
     resources.show()
-elif selected_page == "Profile":
-    profile.show()
